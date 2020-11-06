72d2590c5c2c2a361c1251b3b46187850f7f203c
Admin <jlai003@yahoo.com>
2020-11-07 11:24:42 +1300

    initial styling to homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './homepage.styles.scss';
 
-function Homepage() {
+const Homepage = () => {
     return (
         <div className = 'homepage'>
             <div className = "directory-menu">
